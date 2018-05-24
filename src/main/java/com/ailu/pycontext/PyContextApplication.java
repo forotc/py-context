fbca25f552e8a2a25a560d35c2c411586883ca74
liu zhenming <1119264845@qq.com>
2018-05-24 17:10:15 +0800

    初始化项目构建

---
+++ src/main/java/com/ailu/pycontext/PyContextApplication.java
@@ -0,0 +1,12 @@
+package com.ailu.pycontext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyContextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PyContextApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ailu/pycontext/PyContextApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ailu.pycontext;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PyContextApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
